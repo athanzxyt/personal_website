328a0ae9cbf93d9475d84676dec06cc150ccf547
Athan Zhang <athanzxyt@gmail.com>
2024-01-15 23:29:27 -0500

    ADD: Expand NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,12 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <div className="flex justify-center sm:flex-col sm:pt-20 sm:w-auto">
+        <div className="flex flex-wrap my-4 justify-center sm:flex-col sm:pt-20 sm:w-auto">
             <NavLink exact to="/" className={({isActive}) => isActive ? "nav-item active": "nav-item" }>Home</NavLink>
             <NavLink exact to="/projects" className={({isActive}) => isActive ? "nav-item active": "nav-item" }>Projects</NavLink>
+            <NavLink exact to="/experience" className={({isActive}) => isActive ? "nav-item active": "nav-item" }>Experience</NavLink>
+            <NavLink exact to="/skills" className={({isActive}) => isActive ? "nav-item active": "nav-item" }>Skills</NavLink>
+            <NavLink exact to="/playground" className={({isActive}) => isActive ? "nav-item active": "nav-item" }>Playground</NavLink>
             <NavLink exact to="/contact" className={({isActive}) => isActive ? "nav-item active": "nav-item" }>Contact</NavLink>
         </div>
     )
