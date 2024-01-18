d8433b2a10b56677f2f33f033337c718dea5a887
Athan Zhang <athanzxyt@gmail.com>
2024-01-18 16:21:34 -0500

    FIX: Server side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 // App.jsx
 
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import ContentPanel from './components/ContentPanel';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="bg-background p-5 w-screen h-screen min-h-screen">
-        <div className="border border-primary p-5 h-full w-auto">
-          <div className="h-full 
-                          flex flex-col
-                          sm:grid sm:gap-x-6 sm:grid-cols-2 lg:grid-cols-3">
-            <div className="align-center 
-                            sm:align-start sm:col-span-1">
-              <Header />
-              <NavBar />
-            </div>
-            <div className="flex-grow sm:col-span-1 lg:col-span-2 overflow-y-scroll max-h-full no-scrollbar">
-              <ContentPanel />
-            </div>
+    <div className="bg-background p-5 w-screen h-screen min-h-screen">
+      <div className="border border-primary p-5 h-full w-auto">
+        <div className="h-full 
+                        flex flex-col
+                        sm:grid sm:gap-x-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="align-center 
+                          sm:align-start sm:col-span-1">
+            <Header />
+            <NavBar />
+          </div>
+          <div className="flex-grow sm:col-span-1 lg:col-span-2 overflow-y-scroll max-h-full no-scrollbar">
+            <ContentPanel />
           </div>
         </div>
       </div>
-    </BrowserRouter>
+    </div>
   )
 }
 
