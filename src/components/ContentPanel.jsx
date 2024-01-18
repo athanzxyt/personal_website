635fdb08b440c8e4c67603d4216f5882f6531b91
Athan Zhang <athanzxyt@gmail.com>
2024-01-18 15:46:14 -0500

    ADD: Finish Experience Page

--- src/components/ContentPanel.jsx
+++ src/components/ContentPanel.jsx
@@ -11,7 +11,7 @@ import Contact from '../pages/Contact';
 
 function ContentPanel() {
     return (
-        <div className="flex w-full max-w-full h-full min-h-full justify-center sm:justify-end">
+        <div className="flex w-full max-w-full h-full min-h-full justify-center sm:justify-start">
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/projects" element={<Projects />} />
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -22,27 +22,28 @@ function Experience() {
 
     return (
         <div className="flex flex-col">
-            <h1 className="text-6xl font-syne font-bold text-right custom-grad">Education</h1>
+            <h1 className="text-6xl font-syne font-bold text-left custom-grad pb-4">Education</h1>
             <ul className="list-none p-0 m-0">
                 {education.map(education => (
-                <li key={education.id} className="py-8">
-                    <h2 className="text-3xl sm:text-4xl font-syne font-bold text-right">{education.school}</h2>
-                    <p className="text-base sm:text-xl font-dm text-right">{education.degree}</p>
-                    <p className="text-base sm:text-xl font-dm text-right">{education.gpa}</p>
-                    <p className="text-base sm:text-xl font-dm text-right">{education.year}</p>
-                    <p className="text-base sm:text-xl font-dm text-right">{education.location}</p>
+                <li key={education.id} className="pb-4">
+                    <h2 className="text-3xl sm:text-4xl font-dm font-bold text-left">{education.school}</h2>
+                    <p className="text-base sm:text-xl font-dm text-left">{education.degree}</p>
+                    <p className="text-base sm:text-xl font-dm text-left italic">{education.major}</p>
+                    <p className="text-base font-dm text-left">{education.year}</p>
                 </li>
                 ))}
             </ul>
 
-            <h1 className="text-6xl font-syne font-bold text-right custom-grad">Work History</h1>
+            <h1 className="text-6xl font-syne font-bold text-left custom-grad pt-6 pb-4">Work History</h1>
             <ul className="list-none p-0 m-0">
                 {workHistory.map(workHistory => (
-                <li key={workHistory.id} className="py-8">
-                    <h2 className="text-3xl sm:text-4xl font-syne font-bold text-right">{workHistory.title}</h2>
-                    <p className="text-base sm:text-xl font-dm text-right">{workHistory.company}</p>
-                    <p className="text-base sm:text-xl font-dm text-right">{workHistory.year}</p>
-                    <p className="text-base sm:text-xl font-dm text-right">{workHistory.location}</p>
+                <li key={workHistory.id} className="pb-4">
+                    <h2 className="text-3xl sm:text-4xl font-dm text-left">
+                        {workHistory.title} 
+                        <span className="text-secondary"> @ </span>
+                        <span className="font-bold"> {workHistory.company}</span>
+                    </h2>
+                    <p className="text-base font-dm text-left">{workHistory.location}  |  {workHistory.years}</p>
                 </li>
                 ))}
             </ul>
