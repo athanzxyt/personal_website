23c5f0ea3160dd0cc69ae12b62ed6794b16912ba
Athan Zhang <athanzxyt@gmail.com>
2024-01-19 21:11:30 -0500

    FIX: Responsitivity

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import ContentPanel from './components/ContentPanel';
 
 function App() {
   return (
-    <div className="bg-background p-5 h-svh w-full min-h-screen">
-      <div className="border-2 border-primary p-5 h-full w-auto">
+    <div className="bg-background p-5 min-h-screen sm:h-screen w-full">
+      <div className="flex border-2 border-primary p-5 h-[95vh] sm:h-full w-auto">
         <div className="h-full 
                         flex flex-col
                         sm:grid sm:gap-x-6 sm:grid-cols-2 lg:grid-cols-3">
@@ -17,7 +17,7 @@ function App() {
             <Header />
             <NavBar />
           </div>
-          <div className="flex-grow sm:col-span-1 lg:col-span-2 overflow-y-scroll max-h-full no-scrollbar">
+          <div className="flex-grow sm:col-span-1 lg:col-span-2 sm:overflow-y-scroll max-h-full no-scrollbar">
             <ContentPanel />
           </div>
         </div>
