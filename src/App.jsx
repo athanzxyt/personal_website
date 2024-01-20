c8e14762549064d1222830552c036a3d9528234c
Athan Zhang <athanzxyt@gmail.com>
2024-01-19 20:18:16 -0500

    FIX: Mobile Search Bar Blockage

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import ContentPanel from './components/ContentPanel';
 
 function App() {
   return (
-    <div className="bg-background p-5 w-screen h-screen min-h-screen">
-      <div className="border-2 border-primary p-5 h-full w-auto pb-10 sm:pb-0">
+    <div className="bg-background p-5 pb-10 sm:pb-5 w-screen h-screen min-h-screen">
+      <div className="border-2 border-primary p-5 h-full w-auto">
         <div className="h-full 
                         flex flex-col
                         sm:grid sm:gap-x-6 sm:grid-cols-2 lg:grid-cols-3">
